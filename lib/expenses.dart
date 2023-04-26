89ed923c083cbe1b03080f471b5024b684c795ae
pveg <pveg@live.com>
2023-04-26 20:12:48 +0100

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: const [Text('The chart'), Text('testing')],
+    ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:practice_app/expenses.dart';
+
+
+void main() {
+  runApp(const MaterialApp(
+    home: Expenses(),
+  ));
+}
